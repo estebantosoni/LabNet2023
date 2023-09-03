46c613a29d0efe26467d91d5d3da1307d9765960
estebantosoni <tosoniesteban@gmail.com>
2023-09-03 18:13:01 -0300

    query for the db

---
+++ queryProducts.sql
@@ -0,0 +1,11 @@
+use Northwind
+
+insert into Products (ProductName, UnitsInStock, UnitPrice)
+values 
+	('pan', 3, 2),
+	('leche', 10, 10),
+	('azucar', 0, 8),
+	('manteca', 0, 11),
+	('yerba', 1, 6),
+	('dulce de leche', 0, 4),
+	('caramelo', 30, 1);

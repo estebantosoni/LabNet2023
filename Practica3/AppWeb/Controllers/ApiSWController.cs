e265deb8d9770b722141194121c8972660470e6d
estebantosoni <tosoniesteban@gmail.com>
2023-09-11 20:33:37 -0300

    add public api

---
+++ Practica3/AppWeb/Controllers/ApiSWController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using AppWeb.Models;
+
+namespace AppWeb.Controllers
+{
+    public class ApiSWController : Controller
+    {/*
+        public ActionResult Index()
+        {
+            return View();
+        }*/
+
+        public async Task<ActionResult> Index()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync("https://swapi.dev/api/people");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    dynamic apiResponse = JsonConvert.DeserializeObject(content);
+
+                    if (apiResponse != null && apiResponse.results != null)
+                    {
+                        var starWarsCharacterList = apiResponse.results.ToObject<List<PersonajesSW>>(); ;
+                        return View("Index",starWarsCharacterList);
+                    }
+                }
+                return null;
+            }
+        }
+    }
+}
---
+++ Practica3/AppWeb/Models/PersonajesSW.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppWeb.Models
+{
+    public class PersonajesSW
+    {
+
+        public string Name { get; set; }
+        public int Height { get; set; }
+        public int Mass { get; set; }
+        public string Hair_color { get; set; }
+        public string Skin_color { get; set; }
+        public string Eye_color { get; set; }
+        public string Birth_year { get; set; }
+        public string Gender { get; set; }
+    }
+}
